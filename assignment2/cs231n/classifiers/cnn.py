b63a95488a8d76e1987c8228a797b1934a3780d9
Hyunho Kim <khh0115@gmail.com>
2017-07-26 14:23:04 +0900

    Very first commit

---
+++ assignment2/cs231n/classifiers/cnn.py
@@ -0,0 +1,133 @@
+from builtins import object
+import numpy as np
+
+from cs231n.layers import *
+from cs231n.fast_layers import *
+from cs231n.layer_utils import *
+
+
+class ThreeLayerConvNet(object):
+    """
+    A three-layer convolutional network with the following architecture:
+
+    conv - relu - 2x2 max pool - affine - relu - affine - softmax
+
+    The network operates on minibatches of data that have shape (N, C, H, W)
+    consisting of N images, each with height H and width W and with C input
+    channels.
+    """
+
+    def __init__(self, input_dim=(3, 32, 32), num_filters=32, filter_size=7,
+                 hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=0.0,
+                 dtype=np.float32):
+        """
+        Initialize a new network.
+
+        Inputs:
+        - input_dim: Tuple (C, H, W) giving size of input data
+        - num_filters: Number of filters to use in the convolutional layer
+        - filter_size: Size of filters to use in the convolutional layer
+        - hidden_dim: Number of units to use in the fully-connected hidden layer
+        - num_classes: Number of scores to produce from the final affine layer.
+        - weight_scale: Scalar giving standard deviation for random initialization
+          of weights.
+        - reg: Scalar giving L2 regularization strength
+        - dtype: numpy datatype to use for computation.
+        """
+        self.params = {}
+        self.reg = reg
+        self.dtype = dtype
+
+        ############################################################################
+        # TODO: Initialize weights and biases for the three-layer convolutional    #
+        # network. Weights should be initialized from a Gaussian with standard     #
+        # deviation equal to weight_scale; biases should be initialized to zero.   #
+        # All weights and biases should be stored in the dictionary self.params.   #
+        # Store weights and biases for the convolutional layer using the keys 'W1' #
+        # and 'b1'; use keys 'W2' and 'b2' for the weights and biases of the       #
+        # hidden affine layer, and keys 'W3' and 'b3' for the weights and biases   #
+        # of the output affine layer.                                              #
+        ############################################################################
+        C, H, W = input_dim
+        W1 = np.random.normal(0, weight_scale, [num_filters, 3, filter_size, filter_size])
+        b1 = np.zeros(num_filters)
+        stride = 1
+        pad = (filter_size-1)//2
+        h_conv = (H+2*pad - filter_size) / stride + 1
+        w_conv = (W + 2 * pad - filter_size) / stride + 1
+        ph = 2
+        pw = 2
+        ps = 2
+
+        h_out = int( (h_conv - ph) / ps + 1 )
+        w_out = int( (w_conv - pw) / ps + 1 )
+        W2 = np.random.normal(0, weight_scale, [num_filters * h_out * w_out, hidden_dim])
+        b2 = np.zeros(hidden_dim)
+        W3 = np.random.normal(0, weight_scale, [hidden_dim, num_classes])
+        b3 = np.zeros(num_classes)
+
+
+        self.params.update({'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2, 'W3':W3, 'b3':b3})
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        for k, v in self.params.items():
+            self.params[k] = v.astype(dtype)
+
+
+    def loss(self, X, y=None):
+        """
+        Evaluate loss and gradient for the three-layer convolutional network.
+
+        Input / output: Same API as TwoLayerNet in, 'W2': dw2, 'b2': db2 fc_net.py.
+        """
+        W1, b1 = self.params['W1'], self.params['b1']
+        W2, b2 = self.params['W2'], self.params['b2']
+        W3, b3 = self.params['W3'], self.params['b3']
+
+        # pass conv_param to the forward pass for the convolutional layer
+        filter_size = W1.shape[2]
+        conv_param = {'stride': 1, 'pad': (filter_size - 1) // 2}
+
+        # pass pool_param to the forward pass for the max-pooling layer
+        pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
+
+        scores = None
+        ############################################################################
+        # TODO: Implement the forward pass for the three-layer convolutional net,  #
+        # computing the class scores for X and storing them in the scores          #
+        # variable.                                                                #
+        ############################################################################
+
+        a1, cache1 = conv_relu_pool_forward(X, W1, b1, conv_param, pool_param)
+        N, F, h, w = a1.shape
+        a1 = np.reshape(a1, [N, F*h*w])
+        a2, cache2 = affine_relu_forward(a1, W2, b2)
+        scores, cache3 = affine_forward(a2, W3, b3)
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        if y is None:
+            return scores
+
+        loss, grads = 0, {}
+        ############################################################################
+        # TODO: Implement the backward pass for the three-layer convolutional net, #
+        # storing the loss and gradients in the loss and grads variables. Compute  #
+        # data loss using softmax, and make sure that grads[k] holds the gradients #
+        # for self.params[k]. Don't forget to add L2 regularization!               #
+        ############################################################################
+        loss, df = softmax_loss(scores, y)
+        da1, dw3, db3 = affine_backward(df, cache3)
+        da2, dw2, db2 = affine_relu_backward(da1, cache2)
+        da2 = np.reshape(da2, [N, F, h, w])
+        da3, dw1, db1 = conv_relu_pool_backward(da2, cache1)
+
+        grads.update({'W1': dw1, 'b1': db1, 'W2': dw2, 'b2': db2, 'W3': dw3, 'b3': db3})
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        return loss, grads
---
+++ experiment/datasets/data_utils.py
@@ -0,0 +1,240 @@
+from __future__ import print_function
+
+from builtins import range
+from six.moves import cPickle as pickle
+import numpy as np
+import os, json
+from scipy.misc import imread
+import platform
+import h5py
+import urllib.request, urllib.error, urllib.parse, tempfile
+
+
+BASE_DIR = 'datasets/coco_captioning'
+
+def load_coco_data(base_dir=BASE_DIR,
+                   max_train=None,
+                   pca_features=True):
+    data = {}
+    caption_file = os.path.join(base_dir, 'coco2014_captions.h5')
+    with h5py.File(caption_file, 'r') as f:
+        for k, v in f.items():
+            data[k] = np.asarray(v)
+
+    if pca_features:
+        train_feat_file = os.path.join(base_dir, 'train2014_vgg16_fc7_pca.h5')
+    else:
+        train_feat_file = os.path.join(base_dir, 'train2014_vgg16_fc7.h5')
+    with h5py.File(train_feat_file, 'r') as f:
+        data['train_features'] = np.asarray(f['features'])
+
+    if pca_features:
+        val_feat_file = os.path.join(base_dir, 'val2014_vgg16_fc7_pca.h5')
+    else:
+        val_feat_file = os.path.join(base_dir, 'val2014_vgg16_fc7.h5')
+    with h5py.File(val_feat_file, 'r') as f:
+        data['val_features'] = np.asarray(f['features'])
+
+    dict_file = os.path.join(base_dir, 'coco2014_vocab.json')
+    with open(dict_file, 'r') as f:
+        dict_data = json.load(f)
+        for k, v in dict_data.items():
+            data[k] = v
+
+    train_url_file = os.path.join(base_dir, 'train2014_urls.txt')
+    with open(train_url_file, 'r') as f:
+        train_urls = np.asarray([line.strip() for line in f])
+    data['train_urls'] = train_urls
+
+    val_url_file = os.path.join(base_dir, 'val2014_urls.txt')
+    with open(val_url_file, 'r') as f:
+        val_urls = np.asarray([line.strip() for line in f])
+    data['val_urls'] = val_urls
+
+    # Maybe subsample the training data
+    if max_train is not None:
+        num_train = data['train_captions'].shape[0]
+        mask = np.random.randint(num_train, size=max_train)
+        data['train_captions'] = data['train_captions'][mask]
+        data['train_image_idxs'] = data['train_image_idxs'][mask]
+
+    return data
+
+
+def decode_captions(captions, idx_to_word):
+    singleton = False
+    if captions.ndim == 1:
+        singleton = True
+        captions = captions[None]
+    decoded = []
+    N, T = captions.shape
+    for i in range(N):
+        words = []
+        for t in range(T):
+            word = idx_to_word[captions[i, t]]
+            if word != '<NULL>':
+                words.append(word)
+            if word == '<END>':
+                break
+        decoded.append(' '.join(words))
+    if singleton:
+        decoded = decoded[0]
+    return decoded
+
+
+def sample_coco_minibatch(data, batch_size=100, split='train'):
+    split_size = data['%s_captions' % split].shape[0]
+    mask = np.random.choice(split_size, batch_size)
+    captions = data['%s_captions' % split][mask]
+    image_idxs = data['%s_image_idxs' % split][mask]
+    image_features = data['%s_features' % split][image_idxs]
+    urls = data['%s_urls' % split][image_idxs]
+    return captions, image_features, urls
+
+
+def image_from_url(url):
+    """
+    Read an image from a URL. Returns a numpy array with the pixel data.
+    We write the image to a temporary file then read it back. Kinda gross.
+    """
+    try:
+        f = urllib.request.urlopen(url)
+        _, fname = tempfile.mkstemp()
+        with open(fname, 'wb') as ff:
+            ff.write(f.read())
+        img = imread(fname)
+        os.remove(fname)
+        return img
+    except urllib.error.URLError as e:
+        print('URL Error: ', e.reason, url)
+    except urllib.error.HTTPError as e:
+        print('HTTP Error: ', e.code, url)
+
+
+
+def load_pickle(f):
+    version = platform.python_version_tuple()
+    if version[0] == '2':
+        return  pickle.load(f)
+    elif version[0] == '3':
+        return  pickle.load(f, encoding='latin1')
+    raise ValueError("invalid python version: {}".format(version))
+
+def load_CIFAR_batch(filename):
+    """ load single batch of cifar """
+    with open(filename, 'rb') as f:
+        datadict = load_pickle(f)
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
+        Y = np.array(Y)
+        return X, Y
+
+def load_CIFAR10(ROOT):
+    """ load all of cifar """
+    xs = []
+    ys = []
+    for b in range(1,6):
+        f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
+        X, Y = load_CIFAR_batch(f)
+        xs.append(X)
+        ys.append(Y)
+    Xtr = np.concatenate(xs)
+    Ytr = np.concatenate(ys)
+    del X, Y
+    Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
+    return Xtr, Ytr, Xte, Yte
+
+
+def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000,
+                     subtract_mean=True):
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for classifiers. These are the same steps as we used for the SVM, but
+    condensed to a single function.
+    """
+    # Load the raw CIFAR-10 data
+    cifar10_dir = 'datasets/cifar-10-batches-py'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # Subsample the data
+    mask = list(range(num_training, num_training + num_validation))
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = list(range(num_training))
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = list(range(num_test))
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean image
+    if subtract_mean:
+        mean_image = np.mean(X_train, axis=0)
+        X_train -= mean_image
+        X_val -= mean_image
+        X_test -= mean_image
+
+    # Transpose so that channels come first
+    X_train = X_train.transpose(0, 3, 1, 2).copy()
+    X_val = X_val.transpose(0, 3, 1, 2).copy()
+    X_test = X_test.transpose(0, 3, 1, 2).copy()
+
+    # Package data into a dictionary
+    return {
+      'X_train': X_train, 'y_train': y_train,
+      'X_val': X_val, 'y_val': y_val,
+      'X_test': X_test, 'y_test': y_test,
+    }
+
+
+
+def load_models(models_dir):
+    """
+    Load saved models from disk. This will attempt to unpickle all files in a
+    directory; any files that give errors on unpickling (such as README.txt)
+    will be skipped.
+
+    Inputs:
+    - models_dir: String giving the path to a directory containing model files.
+      Each model file is a pickled dictionary with a 'model' field.
+
+    Returns:
+    A dictionary mapping model file names to models.
+    """
+    models = {}
+    for model_file in os.listdir(models_dir):
+        with open(os.path.join(models_dir, model_file), 'rb') as f:
+            try:
+                models[model_file] = load_pickle(f)['model']
+            except pickle.UnpicklingError:
+                continue
+    return models
+
+
+def load_imagenet_val(num=None):
+    """Load a handful of validation images from ImageNet.
+
+    Inputs:
+    - num: Number of images to load (max of 25)
+
+    Returns:
+    - X: numpy array with shape [num, 224, 224, 3]
+    - y: numpy array of integer image labels, shape [num]
+    - class_names: dict mapping integer label to class name
+    """
+    imagenet_fn = 'cs231n/datasets/imagenet_val_25.npz'
+    if not os.path.isfile(imagenet_fn):
+      print('file %s not found' % imagenet_fn)
+      print('Run the following:')
+      print('cd cs231n/datasets')
+      print('bash get_imagenet_val.sh')
+      assert False, 'Need to download imagenet_val_25.npz'
+    f = np.load(imagenet_fn)
+    X = f['X']
+    y = f['y']
+    class_names = f['label_map'].item()
+    if num is not None:
+        X = X[:num]
+        y = y[:num]
+    return X, y, class_names
